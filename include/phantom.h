70a2329b99a248d766bdee3733ae6644f3294249
Erik Chevalier <erikchevali@gmail.com>
2026-04-14 15:10:54 -0500

    feat: simple circle phantom image

--- include/phantom.h
+++ include/phantom.h
@@ -2,3 +2,7 @@
 #include <vector>
 
 void generatePhantom(std::vector<std::vector<double>>& image, int size);
+void generateCirclePhantom(std::vector<std::vector<double>>& image, int size);
+void generateOffCenterPhantom(std::vector<std::vector<double>>& image, int size);
+void generateHighContrastPhantom(std::vector<std::vector<double>>& image, int size);
+void generateMultiObjectPhantom(std::vector<std::vector<double>>& image, int size);
